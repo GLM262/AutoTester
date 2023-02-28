b883345ab0da62fb4693032963d0f2124862cfb2
Gabe <g.l.m.uk@hotmail.com>
2023-02-28 11:57:00 +0000

    Started writing template processor components

---
+++ main.rb
@@ -0,0 +1,40 @@
+# sections helps generate the test scripts
+# interger tests
+class int_tests
+    def initialize(lower, upper)
+        @lower = lower
+        @upper = upper
+    end
+    def normal_test
+    end
+    def boundary_test
+    end
+    def extreme_test
+    end
+    def erroneous_test
+    end
+end
+# float tests
+class float_tests
+    def initialize(lower, upper)
+        @lower = lower
+        @upper = upper
+    end
+    def normal_test
+    end
+    def boundary_test
+    end
+    def extreme_test
+    end
+    def erroneous_test
+    end
+end
+# string tests
+class string_tests
+    def initialize()
+    end
+    def normal_test
+    end
+    def erroneous_test
+    end
+end
